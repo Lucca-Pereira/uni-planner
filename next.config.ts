6ba2582165a38e14d8409356ca4aaada0a95c3a4
Lucca Pereira <67197943+Lucca-Pereira@users.noreply.github.com>
2025-08-22 17:46:00 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-    ],
+    domains: ["lh3.googleusercontent.com"], // allow Google profile images
   },
 };
 
